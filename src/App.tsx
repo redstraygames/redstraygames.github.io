c5e502a6af069221e5279f1bf660a871d8903d2a
redstraygames <redstraygames@gmail.com>
2025-01-15 23:35:08 +0000

    added proper footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Main } from './Components/Main'
 import { Navbar } from './Components/Navbar'
+import { Footer } from './Components/Footer'
 
 function App() {
   return (
-    <div className='flex flex-col'>
-      <Navbar/>
-      <Routes>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow">
+        <Routes>
           <Route path="/about" element={<div>About Page</div>} />
           <Route path="/games" element={<div>Games Page</div>} />
           <Route path="/assets" element={<div>Assets Page</div>} />
-      </Routes>
-      <Main />
+        </Routes>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
+
 
 export default App
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,8 @@
+import { Socials } from "./Socials";
+
+export function Footer(){
+    return (<footer className="flex place-content-around bg-black p-4">
+    <h1 className="font-roboto text-white">© 2025 All rights reserved.</h1>
+    {/* <Socials/> */}
+    </footer>)
+}
